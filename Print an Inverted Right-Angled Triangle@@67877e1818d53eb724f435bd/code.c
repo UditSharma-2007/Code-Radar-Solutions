0b9e17a6280bbf2e8474e3ec770d1965decd6db9
UditSharma-2007 <uditsharma7517@gmail.com>
2025-03-31 18:02:53 +0530

    Solution to Print an Inverted Right-Angled Triangle

--- Print an Inverted Right-Angled Triangle@@67877e1818d53eb724f435bd/code.c
+++ Print an Inverted Right-Angled Triangle@@67877e1818d53eb724f435bd/code.c
@@ -1,70 +1,28 @@
-// #include <stdio.h>
-
-// int main() {
-//     int n, i, j;
-
-//     // Get the number of rows from the user
-//     printf("Enter the number of rows: ");
-//     scanf("%d", &n);
-
-//     // Loop through each row
-//     for (i = n; i >= 1; i--) {
-//         // Loop to print stars in each row
-//         for (j = 1; j <= i; j++) {
-//             printf("*");
-//         }
-//         // Move to the next line after each row
-//         printf("\n");
-//     }
-
-
-// return 0;
-// }
-
 #include <stdio.h>
 
-int factorial(int n) {
-    if (n == 0 || n == 1) {
-        return 1; // Base case: factorial of 0 or 1 is 1
+void printInvertedTriangle(int n) {
+    // Outer loop for rows, N se 1 tak
+    for (int i = n; i >= 1; i--) {
+        // Inner loop for printing stars in each row
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        // New line after each row
+        printf("\n");
     }
-    return n * factorial(n - 1); // Recursive call
 }
 
 int main() {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
+    int n;
+    printf("Enter the number of rows (N): ");
+    scanf("%d", &n);
     
-    if (num < 0) {
-        printf("Factorial is not defined for negative numbers.\n");
+    // Agar n negative ya zero hai toh invalid input
+    if (n <= 0) {
+        printf("Please enter a positive number.\n");
     } else {
-        int result = factorial(num);
-        printf("%d\n", result);
+        printInvertedTriangle(n);
     }
+    
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
